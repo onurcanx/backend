f8055da25dcef72929d5596dbdb3011cc1756a1b
onurcanx <onuraa79@gmail.com>
2025-05-05 21:26:41 +0300

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 require("dotenv").config();
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
