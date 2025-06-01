81609f687913a7b982e04a165aacb6ec301fa183
onurcanx <onuraa79@gmail.com>
2025-06-01 21:02:14 +0300

    Update coommentsroutesanalyzer server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const axios = require('axios');
+const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
@@ -8,18 +8,19 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // JSON verileri almak için
+app.use(express.json());
 
 // Rotalar
 const authRoutes = require("./routes/authRoutes");
+const commentRoutes = require("./routes/commentRoutes");
 
 app.use("/api/auth", authRoutes);
+app.use("/api/analyzer", commentRoutes);
 
 // Test endpoint
 app.get("/", (req, res) => {
   res.send("Server çalışıyor!");
 });
-
 
 app.listen(port, () => {
   console.log(`Server ${port} numaralı portta çalışıyor`);
