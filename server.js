c9c9a7359044169e63139ce68b34e33421d69f57
onurcanx <onuraa79@gmail.com>
2025-06-02 12:20:48 +0300

    Revert "Update coommentsroutesanalyzer server.js"
    This reverts commit 81609f687913a7b982e04a165aacb6ec301fa183.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
+const axios = require('axios');
 require("dotenv").config();
 
 const app = express();
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // JSON verileri almak için
 
 // Rotalar
 const authRoutes = require("./routes/authRoutes");
@@ -19,6 +19,7 @@ app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("Server çalışıyor!");
 });
+
 
 app.listen(port, () => {
   console.log(`Server ${port} numaralı portta çalışıyor`);
